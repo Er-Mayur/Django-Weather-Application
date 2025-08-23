b66d82a9376eab0cd1baf0324987e9dec6d12426
Mayur Mahajan <mahajanmayur2111@gmail.com>
2025-08-23 23:16:22 +0530

    first commit

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 set -e
-
-# Install deps (Vercel also installs automatically, this ensures local parity if needed)
 pip install -r requirements.txt
-
-# Collect static files into ./staticfiles
 python manage.py collectstatic --noinput
 
